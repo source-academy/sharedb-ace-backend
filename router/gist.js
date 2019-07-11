2e8df305996f8578bd4f1bb00dd4ba3a2097a275
zephyr <shejiayu1998@qq.com>
2019-07-12 03:15:14 +0800

    Set up linter

--- router/gist.js
+++ router/gist.js
@@ -5,35 +5,38 @@ import HashSet from './hashset.js';
 
 const router = new Router();
 let latest = null;
-let list_id = new HashSet();
+const listId = new HashSet();
 let count = 0;
 
+/**
+ * Creates a new session.
+ */
 async function newGist() {
   const id = uuid();
   const doc = share.connect().get('codepad', id);
   await new Promise( (resolve, reject) => {
     doc.create({
-      "code": "// Type your program in here!"
+      'code': '// Type your program in here!',
     }, function(err) {
       if (err) reject(err);
       latest = id;
       count++;
-      list_id.put(id);
+      listId.put(id);
       resolve();
     });
   });
-  console.log("=================================");
+  console.log('=================================');
   console.log(`New session created with uuid: ${id}`);
-  console.log("In newGist(), count = " + count);
-  console.log("lastest: " + latest);
+  console.log('In newGist(), count = ' + count);
+  console.log('lastest: ' + latest);
   return id;
 }
 
 // Statistics
 router.get('/stats', async (ctx) => {
-  console.log("=================================");
-  console.log("Obtaining stats...");
-  console.log("In gists/stats, count = " + count);
+  console.log('=================================');
+  console.log('Obtaining stats...');
+  console.log('In gists/stats, count = ' + count);
   ctx.body = {count: count};
 });
 
@@ -43,29 +46,29 @@ newGist();
 // Gists
 router.post('/gists/new', async (ctx) => {
   const id = await newGist();
-  console.log("=================================");
-  console.log("New session...");
-  console.log("In gists/new, count = " + count);
+  console.log('=================================');
+  console.log('New session...');
+  console.log('In gists/new, count = ' + count);
   ctx.body = {id};
 });
 
 router.get('/gists/latest', async (ctx) => {
   newGist();
-  console.log("=================================");
-  console.log("Obtaining latest created session...");
-  console.log("In gists/latest, count = " + count);
-  console.log("lastest: " + latest);
+  console.log('=================================');
+  console.log('Obtaining latest created session...');
+  console.log('In gists/latest, count = ' + count);
+  console.log('lastest: ' + latest);
   ctx.body = {id: latest};
 });
 
 router.get('/gists/:sid', async (ctx) => {
   const id = ctx.params.sid;
-  const flag = list_id.contains(id);
-  console.log("=================================");
+  const flag = listId.contains(id);
+  console.log('=================================');
   console.log(`Accessing session with uuid: ${ctx.params.sid}`);
-  console.log("In gists/:sid, count = " + count);
-  console.log("lastest: " + latest);
-  console.log("Session fould: " + flag);
+  console.log('In gists/:sid, count = ' + count);
+  console.log('lastest: ' + latest);
+  console.log('Session fould: ' + flag);
   ctx.body = {id: ctx.params.sid, state: flag};
 });
 
